3c124c11050bdb0541838048b3e3a58fcc3bb28a
Anton Zikratski <zikratskianton@gmail.com>
2023-06-27 10:58:36 +0300

    write main function getting solutions

--- algo.py
+++ algo.py
@@ -1,7 +1,19 @@
 import numpy as np
 
 matr = np.zeros((3,3))
-print(matr)
+#print(matr)
 
-def AI_get_solution(matr):
-    pass
+
+
+def AI_get_solutions(matr):
+    columns_len = len(matr[0])
+    rows_len = len(matr)
+    solutions = []
+    #win_lines - здесь будем хранить для каждого возможного решения(тройки) координаты
+    win_lines = {f"c{i}":tuple() for i in range(rows_len)} | {f"r{i}":tuple() for i in range(columns_len)} | {f"d{i}":tuple() for i in range(2)}
+    #print(win_lines)
+
+
+    return solutions
+
+AI_get_solutions(matr)
