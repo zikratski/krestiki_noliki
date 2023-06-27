c4a7c6272930f9244c381834908a2f1a1e16e58e
Anton Zikratski <zikratskianton@gmail.com>
2023-06-27 11:38:26 +0300

    write main recursive function

--- algo.py
+++ algo.py
@@ -9,11 +9,36 @@ def AI_get_solutions(matr):
     columns_len = len(matr[0])
     rows_len = len(matr)
     solutions = []
+    state = matr[:] # копируем матрицу в state
     #win_lines - здесь будем хранить для каждого возможного решения(тройки) координаты
-    win_lines = {f"c{i}":tuple() for i in range(rows_len)} | {f"r{i}":tuple() for i in range(columns_len)} | {f"d{i}":tuple() for i in range(2)}
+    win_lines = {f"c{i}":np.array([]) for i in range(rows_len)} | {f"r{i}":np.array([]) for i in range(columns_len)} | {f"d{i}":np.array([]) for i in range(2)}
     #print(win_lines)
-
+    search_solutions(state,solutions)
 
     return solutions
 
-AI_get_solutions(matr)
+def search_solutions(state,solutions):
+    if check_state(state):
+        pass
+    else:
+        candidates = get_candidates(state)
+        for c_move in candidates:
+            matr[c_move[0]][c_move[1]] = 1
+            for p_move in candidates:
+                if p_move != c_move:
+                    matr[c_move[0]][c_move[1]] = 2
+                    search_solutions(state,solutions)
+                    matr[c_move[0]][c_move[1]] = 0
+            matr[c_move[0]][c_move[1]] = 0
+
+def check_state(state):
+    ...
+
+def get_candidates(state):
+    candidates = np.array([])
+    for i in matr:
+        for j in matr[i]:
+            if matr[i][j] == 0:
+                candidates += [(i,j)]
+
+    return candidates
