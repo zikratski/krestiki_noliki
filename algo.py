67c9eb652450702bbfd6d93264e984fb97d3f2c2
Anton Zikratski <zikratskianton@gmail.com>
2023-06-27 16:46:28 +0300

    new commit

--- algo.py
+++ algo.py
@@ -13,14 +13,14 @@ def AI_get_solutions(matr):
     #win_lines - здесь будем хранить для каждого возможного решения(тройки) координаты
     win_lines = {f"c{i}":np.array([0,set(),0]) for i in range(rows_len)} \
                 | {f"r{i}":np.array([0,set(),0]) for i in range(columns_len)} \
-                | {f"d{i}":np.array([0,set()],0) for i in range(2)}
+                | {f"d{i}":np.array([0,set(),0]) for i in range(2)}
     #print(win_lines)
     search_solutions(state,solutions,win_lines)
 
     return solutions
 
 def search_solutions(state,solutions,win_lines):
-    if check_state(state):
+    if check_state(state, solutions, win_lines):
         return
     else:
         candidates = get_candidates(state)
@@ -39,7 +39,7 @@ def search_solutions(state,solutions,win_lines):
                     win_lines[f"r{p_move[1]}"][0] += 1
                     win_lines[f"r{p_move[1]}"][1].add(p_move)
 
-                    search_solutions(state,solutions,win_lines)
+                      search_solutions(state,solutions,win_lines)
 
                     matr[p_move[0]][p_move[1]] = win_lines[f"c{p_move[0]}"][2] = win_lines[f"r{p_move[1]}"][2] = 0
                     win_lines[f"c{p_move[0]}"][0] -= 1
@@ -47,7 +47,7 @@ def search_solutions(state,solutions,win_lines):
                     win_lines[f"r{p_move[1]}"][0] -= 1
                     win_lines[f"r{p_move[1]}"][1].remove(p_move)
 
-            matr[c_move[0]][c_move[1]] = win_lines[f"c{c_move[0]}"][2] = win_lines[f"r{c_move[1]}"][2] = 0
+            #matr[c_move[0]][c_move[1]] = win_lines[f"c{c_move[0]}"][2] = win_lines[f"r{c_move[1]}"][2] = 0
             win_lines[f"c{c_move[0]}"][0] -= 1
             win_lines[f"c{c_move[0]}"][1].remove(c_move)
             win_lines[f"r{c_move[1]}"][0] -= 1
@@ -61,10 +61,12 @@ def check_state(state, solutions, win_lines):
     return False
 
 def get_candidates(state):
-    candidates = np.array([])
-    for i in matr:
-        for j in matr[i]:
-            if matr[i][j] == 0:
-                candidates += [(i,j)]
-
+    candidates = []
+    for i, column in enumerate(state):
+        for j, elem in enumerate(column):
+            if elem == 0:
+                candidates.append(tuple([i,j]))
     return candidates
+
+
+sols = AI_get_solutions(matr)
