8dfe9c453a621823f895c8163398491d0730e5a0
Anton Zikratski <zikratskianton@gmail.com>
2023-06-28 22:14:58 +0300

    white main minmax algorythm

--- algo2.py
+++ algo2.py
@@ -1,4 +1,9 @@
 import numpy as np
+import algo
+
+ai = 1
+pers = 2
+
 def best_move(state):
     inf = float('inf')
     best_score = -inf
@@ -7,15 +12,49 @@ def best_move(state):
     for i, column in enumerate(state):
         for j, elem in enumerate(column):
             if state[i][j] == 0:
-                state[i][j] = 1
-                score = minimax(state, 0, False)
+                state[i][j] = ai
+                score = minimax(state, 0, True)
                 state[i][j] = 0
                 if (score > best_score):
                     best_score = score
                     move = (i,j)
-    state[move[0]][move[1]] = 1
-def minimax(state, smth = 0, b = False):
-    return 1
+    state[move[0]][move[1]] = ai
+def minimax(state, depth, isMaximazing):
+    win_ai = algo.check_win(state)
+    win_per = algo.check_lose(state)
+    win_tie = algo.check_tie(state)
+    if win_ai:
+      return 1
+    elif win_per:
+        return -1
+    elif win_tie:
+        return 0
+
+    if isMaximazing:
+        inf = float('inf')
+        best_score = -inf
+        score = None
+        for i, column in enumerate(state):
+            for j, elem in enumerate(column):
+                if state[i][j] == 0:
+                    state[i][j] = ai
+                    score = minimax(state, depth+1, False)
+                    state[i][j] = 0
+                    best_score = max(score,best_score)
+        return best_score
+    else:
+        inf = float('inf')
+        best_score = inf
+        score = None
+        for i, column in enumerate(state):
+            for j, elem in enumerate(column):
+                if state[i][j] == 0:
+                    state[i][j] = ai
+                    score = minimax(state, depth + 1, True)
+                    state[i][j] = 0
+                    best_score = min(score, best_score)
+        return best_score
+
 
 matr = np.array([[1,0,0],[2,0,1],[1,0,2]])
 
@@ -24,5 +63,4 @@ for _ in range(4):
     best_move(matr)
     print()
     print(matr)
-
 
