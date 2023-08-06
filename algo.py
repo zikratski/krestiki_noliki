9915ada17cfdbb16949580330d659c630947640a
Anton <zikratskianton@gmail.com>
2023-08-06 11:32:33 +0300

    убрал ненужные функции в algo.py, написал к ним комментарии

--- algo.py
+++ algo.py
@@ -1,59 +1,7 @@
 import random
 import numpy as np
 
-
-
-
-
-def AI_get_solutions(matr, mode='standart'):
-    columns_len = len(matr[0])
-    rows_len = len(matr)
-    solutions = []
-    loses = []
-    state = matr[:]
-    solution = None
-    if mode == 'standart':
-        plays = list()
-        search_solutions(state,plays,solutions, loses)
-        print(f"len solutions: {len(solutions)}")
-        solution = random.choice(solutions)[0]
-    elif mode == 'random':
-        solution = random_sol(state)
-    elif mode == 'minmax':
-        solution = best_move(state)
-    return solution
-
-def random_sol(state):
-    sols = []
-    for i, column in enumerate(state):
-        for j, elem in enumerate(column):
-            if state[i][j] == 0:
-                sols.append((i,j))
-    return random.choice(sols)
-
-def search_solutions(state,plays,solutions, loses):
-    candidates = get_candidates(state)
-    for c_move in candidates:
-        state[c_move[0]][c_move[1]] = 1
-        plays.append(c_move)
-        for p_move in candidates:
-            if p_move != c_move:
-                plays.append(p_move)
-                state[p_move[0]][p_move[1]] = 2
-                if check_win(state):
-                    solutions.append(plays)
-                elif check_lose(state):
-                    loses.append(plays)
-                else:
-                    plays = search_solutions(state,plays,solutions,loses)
-
-                plays = plays[:-1]
-                state[p_move[0]][p_move[1]] = 0
-        state[c_move[0]][c_move[1]] = 0
-        plays = plays[:-1]
-    return plays
-
-
+# проверка матрицы на выигрыш
 def check_win(state, ai=1):
     for column in state:
         if np.array_equal(column, np.array([ai,ai,ai])):
@@ -69,6 +17,7 @@ def check_win(state, ai=1):
     else:
         return False
 
+# проверка матрицы на проигрыш
 def check_lose(state,pers=2):
     for column in state:
         if np.array_equal(column, np.array([pers,pers,pers])):
@@ -84,23 +33,17 @@ def check_lose(state,pers=2):
     else:
         return False
 
+# проверка матрицы на ничью
 def check_tie(state,ai=1,pers=2):
     if 0 not in state and not check_lose(state,pers) and not check_win(state,ai):
         return True
     else:
         return False
-def get_candidates(state):
-    candidates = []
-    for i, column in enumerate(state):
-        for j, elem in enumerate(column):
-            if elem == 0:
-                candidates.append(tuple([i,j]))
-    return candidates
 
-
+# выдает лучший ход на данный момент в зависимости от выбора режима сложности
 def best_move(state, mode='extreme',pers=2,ai=1):
     inf = float('inf')
-    score = None
+    #score = None
     move = None
     if mode == 'extreme':
         best_score = -inf
@@ -131,8 +74,8 @@ def best_move(state, mode='extreme',pers=2,ai=1):
     if mode == 'random':
         move = random_sol(state)
     return move
+# основной minimax алгоритм оптимального решения
 def minimax(state, depth, isMaximazing, pers, ai):
-    # if depth == 1:
     win_ai = check_win(state, ai=ai)
     win_per = check_lose(state,pers=pers)
     win_tie = check_tie(state,ai=ai,pers=pers)
@@ -150,7 +93,7 @@ def minimax(state, depth, isMaximazing, pers, ai):
     if isMaximazing:
         inf = float('inf')
         best_score = -inf
-        score = None
+        #score = None
         for i, column in enumerate(state):
             for j, elem in enumerate(column):
                 if state[i][j] == 0:
@@ -164,7 +107,7 @@ def minimax(state, depth, isMaximazing, pers, ai):
     else:
         inf = float('inf')
         best_score = inf
-        score = None
+        #score = None
         for i, column in enumerate(state):
             for j, elem in enumerate(column):
                 if state[i][j] == 0:
@@ -176,6 +119,7 @@ def minimax(state, depth, isMaximazing, pers, ai):
                     best_score = min(score, best_score)
         return best_score
 
+# обратный minimax алгоритм проигрышного решения
 def minimax_lose(state, depth, isMaximazing,pers,ai):
     win_ai = check_win(state,ai)
     win_per = check_lose(state,pers)
@@ -215,7 +159,16 @@ def minimax_lose(state, depth, isMaximazing,pers,ai):
                         return score
                     best_score = min(score, best_score)
         return best_score
+# выбор рандомного шага
+def random_sol(state):
+    sols = []
+    for i, column in enumerate(state):
+        for j, elem in enumerate(column):
+            if state[i][j] == 0:
+                sols.append((i,j))
+    return random.choice(sols)
 
+# получение статистики
 def get_stats(message,state, move='ai',pers=2,ai=1):
     chances = {'ai': 0, 'person': 0, 'tie': 0}
     get_all_possibles(state, chances, move=move,pers=pers,ai=ai)
@@ -229,7 +182,7 @@ def get_stats(message,state, move='ai',pers=2,ai=1):
     #       f"person: {chances['person']}%\n"
     #       f"tie: {chances['tie']}%\n")
 
-
+# просчет всех ситуаций и учет выигрышей/проигрышей/ничей
 def get_all_possibles(state, chances, move,pers,ai):
     win_ai = check_win(state,ai)
     win_per = check_lose(state,pers)
@@ -260,9 +213,4 @@ def get_all_possibles(state, chances, move,pers,ai):
                     get_all_possibles(state, chances, move='ai',pers=pers,ai=ai)
                     state[i][j] = 0
         return
-if __name__ == '__main__':
-    matr = np.zeros((3, 3))
-    #print(matr)
-    solution = AI_get_solutions(matr, mode = 'minmax')
-    print('i am here')
-    print(solution)
+
